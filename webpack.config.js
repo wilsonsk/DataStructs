a7bda5d7042cbe4146b07360cc24cbf15ad3d2dc
Sky <wilson.skylerm@gmail.com>
2017-12-04 06:19:43 -0800

    Add webpack for transpile to ES6

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const config = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}
+		]
+	}
+};
+
+module.exports = config;
